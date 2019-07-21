ddd806a2a2b8befc099f96cd9038ae6303547fc2
derekzhang <pi*7=7.17>
2019-07-21 14:27:46 +0800

    add code for 7-21

---
+++ 2019/2019down/c++/dad/263A.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+using namespace std;
+int abs(int x)
+{
+	return x >= 0 ? x : -x;
+}
+int main()
+{
+	int x,y;
+	for(int i = 1;i <= 5; ++i)
+	{
+		for(int j = 1;j <= 5; ++j)
+		{
+			int p;
+			scanf("%d",&p);
+			if(p == 1)
+			{
+				x = i,y = j;
+			}
+		}
+	}
+	printf("%d\n",abs(x - 3) + abs(y - 3));
+	return 0;
+}
